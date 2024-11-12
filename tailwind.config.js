6f8ff3b844fae995d9a30b85693499acdc2378af
Md. Sarwar Alam <sarwaralam.ini@gmail.com>
2024-11-12 23:19:01 +0600

    Custom Letter Spacing Negative Values

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,12 @@ module.exports = {
       },
       fontWeight: {
         "custom-weight": '100'
+      },
+      letterSpacing: {
+        '1': '0em',
+        '2': '0.025em',
+        '3': '0.05em',
+        '4': '0.1em',
       }
     },
   },
